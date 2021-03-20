dd9143bb36582d1aa9ae28a0062f1639e4316cc4
ShirokoMax <56560768+ShirokoMax@users.noreply.github.com>
2021-03-20 15:58:12 +0300

    добавляет javascript к форме обратной связи

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const feedbackLink = document.querySelector('.contacts-button');
+const feedbackPopup = document.querySelector('.modal-feedback');
+const feedbackClose = feedbackPopup.querySelector('.modal-close');
+const feedbackForm = feedbackPopup.querySelector('.feedback-form');
+const feedbackName = feedbackPopup.querySelector('#feedback-name');
+const feedbackEmail = feedbackPopup.querySelector('#feedback-email');
+const feedbackText = feedbackPopup.querySelector('#feedback-text');
+
+let isStorageSupport = true;
+let storage = '';
+
+try {
+  storage = localStorage.getItem('name');
+} catch(error) {
+  isStorageSupport = false;
+}
+
+feedbackLink.addEventListener('click', event => {
+  event.preventDefault();
+  feedbackPopup.classList.add('modal-show');
+
+  if(storage) {
+    feedbackName.value = localStorage.getItem('name');
+    feedbackEmail.value = localStorage.getItem('email');
+    feedbackText.focus();
+  } else {
+    feedbackName.focus();
+  }
+});
+
+feedbackClose.addEventListener('click', event => {
+  event.preventDefault();
+  feedbackPopup.classList.remove('modal-show');
+  feedbackPopup.classList.remove('modal-error');
+});
+
+feedbackForm.addEventListener('submit', event => {
+  if(!feedbackName.value || !feedbackEmail.value || !feedbackText.value) {
+    event.preventDefault();
+    feedbackPopup.classList.remove('modal-error');
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+    feedbackPopup.classList.add('modal-error');
+  } else {
+    if(isStorageSupport) {
+      localStorage.setItem('name', feedbackName.value);
+      localStorage.setItem('email', feedbackEmail.value);
+    }
+  }
+});
+
+window.addEventListener('keydown', event => {
+  if(event.keyCode === 27) {
+    if(feedbackPopup.classList.contains('modal-show')) {
+      event.preventDefault();
+      feedbackPopup.classList.remove('modal-show');
+      feedbackPopup.classList.remove('modal-error');
+    }
+  }
+});
